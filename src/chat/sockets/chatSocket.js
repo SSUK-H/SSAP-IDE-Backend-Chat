a910bd352e16cd56ca804deaad9c6eb465e123da
Seongsuk Hong <ssukh77@naver.com>
2023-10-09 03:24:23 +0900

    feat: 소켓 접속자 인증 및 설정, 메세지 DB저장 수정, 연결 종료 처리 추가: #14
    setUserInfo로 클라이언트에서 보낸 email과 name을 받아 접속자 확인을 진행하고 joinTime 이후 채팅 데이터 불러오는 로직은 나중에 session 로직에서 따로 처리하기 위해 삭제하고 email 데이터를 받아와서 방에 입장한 사용자를 확인했습니다. sendMessage에서 채팅 데이터 받아 DB 저장하는 saveMessageToDB 호출해서 저장하도록 수정하였으며, 연결 종료 시 처리하는 로직을 추가하였습니다.

--- src/chat/sockets/chatSocket.js
+++ src/chat/sockets/chatSocket.js
@@ -1,61 +1,53 @@
 const { pool } = require("../models/chatModel");
+const { saveMessageToDB } = require("../services/messageService.js");
 
 // 소켓 연결 설정
 const chatSocket = (io) => {
   io.on("connection", (socket) => {
-    console.log("New client connected");
+    console.log("새로운 클라이언트가 연결되었습니다");
 
-    // 접속자 설정
-    socket.on("setUsername", (userName) => {
-      if (!socket) console.error("Socket is undefined!");
-      if (typeof userName === "undefined") console.error("userName is undefined!");
+    // 접속자 인증 맟 접속자 설정
+    socket.on("setUserInfo", ({ email, name }) => {
+      if (!socket) console.error("소켓이 정의되지 않았습니다!");
+      if (typeof name === "undefined" && typeof email === "undefined")
+        console.error("사용자 이름 또는 이메일이 정의되지 않았습니다!");
 
-      socket.userName = userName;
-      console.log(`Username set to ${userName}`);
+      socket.name = name;
+      socket.email = email;
+      console.log(`유저 정보 설정:  이메일 - ${email}, 이름 - ${name}`);
     });
 
     // 컨테이너 고유의 room에 조인
-    socket.on("joinRoom", async (roomId, joinTime) => {
+    socket.on("joinRoom", async (roomId, email) => {
+      socket.join(roomId);
+      console.log(`${roomId}룸에 ${email}님이 입장하셨습니다`);
+    });
+
+    // 메세지 수신 및 해당 room에 보내기
+    socket.on("sendMessage", async (roomId, msg) => {
+      const socketId = socket.id;
+      const email = msg.email;
+      const name = msg.name;
+      const message = msg.message;
+      console.log("메세지를 수신하였습니다: ", roomId, socketId, email, name, message);
+
+      // 메세지를 저장하고 브로드캐스팅
       try {
-        socket.join(roomId);
-        console.log(
-          `User with socket ID ${socket.id} joined room: ${roomId} joinTime: ${joinTime}`,
-        );
+        // DB 저장 호출
+        const result = await saveMessageToDB(roomId, socketId, email, name, message);
 
-        // joinTime 이후의 메세지만 불러오기
-        const [rows] = await pool.query(
-          "SELECT * FROM chat WHERE roomId = ? AND sendTime > ?",
-          [roomId, joinTime],
-        );
-
-        // 클라이언트에게 이전 메시지 보내기
-        socket.emit("previousMessages", rows);
+        if (result.success) {
+          io.to(roomId).emit("receiveMessage", { ...message, socketId });
+          console.log("메세지가 저장되고 다른 클라이언트에게 전송되었습니다");
+        }
       } catch (error) {
-        console.log("Error loading previous messages: ", error);
+        console.log("메세지 저장 중 오류 발생: ", error);
       }
     });
 
-    // 메시지 수신 및 해당 room에 보내기
-    socket.on("sendMessage", async (roomId, socketId, message) => {
-      console.log("Received message:", roomId, socketId, message);
-
-      try {
-        // 메세지 DB에 저장
-        await pool.query(
-          "INSERT INTO chat (roomId, socketId, userName, content) VALUES (?, ?, ?, ?)",
-          [roomId, socketId, message.userName, message.content],
-        );
-
-        // 다른 클라이언트에게 메세지 전송
-        io.to(roomId).emit("receiveMessage", { ...message, socketId });
-        console.log("Message saved and sent to other clients");
-      } catch (error) {
-        console.log("Error saving message: ", error);
-      }
-    });
-
+    // 연결 종료 시 처리
     socket.on("disconnect", () => {
-      console.log(`User with socket ID ${socket.id}`);
+      console.log(`소켓 ID가 ${socket.id}인 사용자가 연결을 해제하였습니다`);
     });
   });
 };
