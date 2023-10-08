d7a1fc91c115e7d8380591d035bc313fe68b40f0
Seongsuk Hong <ssukh77@naver.com>
2023-10-09 02:15:20 +0900

    feat: 채팅 메시지 저장 로직 모듈화: #12
    DB에 채팅 메세지 저장하는 로직을 추가하고 재사용을 위해 로직을 따로 분리했습니다.
    
    칼럼명과 동일하게 작성 필요 ***

---
+++ src/chat/services/messageService.js
@@ -0,0 +1,17 @@
+const { pool } = require("../../../src/chat/models/chatModel");
+
+const saveMessageToDB = async (roomId, socketId, email, name, message) => {
+  try {
+    await pool.query(
+      "INSERT INTO chat (roomId, socketId, email, name, message) VALUES (?, ?, ?, ?, ?)",
+      [roomId, socketId, email, name, message],
+    );
+    console.log("메시지를 DB에 저장 중: ", { roomId, socketId, email, name, message });
+    return { success: true };
+  } catch (error) {
+    console.error("DB에 메시지 저장 중 오류 발생: ", error.message);
+    throw new Error("서버 오류");
+  }
+};
+
+module.exports = { saveMessageToDB };
