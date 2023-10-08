a0baa11db2740917c036a445937f1bbfb848f47b
Seongsuk Hong <ssukh77@naver.com>
2023-10-09 02:22:59 +0900

    feat: 채팅 메세지 불러오는 API 추가: #13
    채팅 메세지 불러오는 getMessages API 로직 추가 라우터 처리하였습니다. 그리고 DB에 roomId가 같은 데이터 중에 접속 시간 이후 데이터들을 불러오도록 요청했습니다.

--- src/chat/routes/chat.js
+++ src/chat/routes/chat.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const { pool } = require("../models/chatModel");
 const router = express.Router();
-const chatHistory = require("../data/chatHistory");
 
-router.get("/", (req, res) => {
-  console.log("sendMessage", chatHistory);
-  res.status(200).send(chatHistory);
+router.get("/getMessages", async (req, res) => {
+  const { roomId } = req.query;
+
+  try {
+    const [rows] = await pool.query(
+      "SELECT * FROM chat WHERE roomId = ? AND sendTime > ?",
+      [roomId, joinTime],
+    );
+    res.status(200).send(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("서버 오류");
+  }
 });
 
 module.exports = router;
