27434cab399ff91d6e3fd010310dd8e65e58b2c9
Seongsuk Hong <ssukh77@naver.com>
2023-10-09 12:44:56 +0900

    fix: session에 저장한 데이터 불러오지 못하는 오류: #19
    getMessages에서 session에 저장된 데이터를 불러오지 못합니다.
    Socket.IO에서는 Express 세션과 관련된 데이터를 사용할 수 있게 설정이 필요했던 것 같습니다.
    express-socket.io-session 미들웨어를 사용해서 Socket.IO와 Express 세션 공유되도록 하고 세션 데이터가 변경될 때 자동으로 저장하도록 설정하였습니다.

--- chat/server.js
+++ chat/server.js
@@ -2,18 +2,20 @@ const express = require("express"); // 웹서버 프레임워크
 const http = require("http");
 const socketIo = require("socket.io");
 const cors = require("cors");
-const expressSession = require("express-session");
+const chatRouter = require("./src/routes/chat");
+const chatSocket = require("./src/sockets/chatSocket");
+const { checkConnection } = require("./src/models/chatModel");
+
 const crypto = require("crypto"); // 랜덤 문자열 생성
-const chatRouter = require("./chat/routes/chat");
-const chatSocket = require("./chat/sockets/chatSocket");
-const { checkConnection } = require("./chat/models/chatModel");
+const expressSession = require("express-session");
+const sharedsession = require("express-socket.io-session");
 
 const CLIENT_ORIGIN = "http://localhost:3000";
 const secretKey = crypto.randomBytes(32).toString("hex");
 const session = expressSession({
   secret: secretKey, // 세션 데이터 암호화 키
-  resave: false,  // 세션 데이터 변경 시 다시 저장 여부
-  saveUninitialized: true,  // 초기화되지 않은 데이터 저장 여부
+  resave: false, // 세션 데이터 변경 시 다시 저장 여부
+  saveUninitialized: true, // 초기화되지 않은 데이터 저장 여부
 });
 
 // 서버 및 소켓 초기화
@@ -25,7 +27,7 @@ app.use(
     origin: CLIENT_ORIGIN,
     methods: ["GET", "POST"],
     credentials: true,
-  })
+  }),
 );
 
 const server = http.createServer(app);
@@ -37,6 +39,12 @@ const io = socketIo(server, {
     credentials: true,
   },
 });
+io.use(
+  // Socket.IO와 Express 세션 공유 설정
+  sharedsession(session, {
+    autoSave: true,
+  }),
+);
 
 // JSON 요청 본문 파싱을 위한 미들웨어
 app.use(express.json());
@@ -54,7 +62,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
 
   const { email, name } = req.headers; // 헤더에서 필요한 정보를 읽어오기
-  req.session.email = email;  // req.session에 세션 정보를 저장
+  req.session.email = email; // req.session에 세션 정보를 저장
   req.session.name = name;
 
   // 세션 데이터를 저장하려면 반드시 다음 미들웨어로 이동해야 함
